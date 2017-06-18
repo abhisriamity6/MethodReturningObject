afdab8b6c781a3ab3ac0bc3e8b9c39a2e068469a
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-18 10:03:34 +0530

    Add project files.

---
+++ MethodReturningObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodReturningObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
